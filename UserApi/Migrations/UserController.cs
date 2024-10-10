ddd4ba603988c4627ecc403c93156a9bafcc3056
szabodanie <szabod@kkszki.hu>
2024-10-10 10:48:35 +0200

    Get and Post empoin is are ready

--- UserApi/Migrations/UserController.cs
+++ UserApi/Migrations/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using UserApi.Models;
+using static UserApi.Models.Dto;
 
 namespace UserApi.Migrations
 {
@@ -16,6 +17,28 @@ namespace UserApi.Migrations
                 return Ok(context.NewUsers.ToList());
             }
               
+        }
+        [HttpPost]
+        public ActionResult<User> Post(CreateUserDto craeteUserDto) 
+        {
+            using (var context = new UserDbContext())
+            {
+                var user = new User 
+                {
+                    Id = Guid.NewGuid(),
+                    Name = craeteUserDto.Name,
+                    Age = craeteUserDto.Age,
+                    License = craeteUserDto.License,
+                };
+
+                if (user != null)
+                {
+                    context.NewUsers.Add(user);
+                    context.SaveChanges();
+                    return StatusCode(201, user);
+                }
+                return BadRequest();
+            }
         }
     }
 }
---
+++ UserApi/Models/Dto.cs
@@ -0,0 +1,7 @@
+﻿namespace UserApi.Models
+{
+    public class Dto
+    {
+        public record CreateUserDto(string Name, int Age, bool License);
+    }
+}
